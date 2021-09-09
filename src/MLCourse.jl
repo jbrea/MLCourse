3d053fd66911f6669335dfcd2c91169448e8a1b1
jbrea <jbrea@users.noreply.github.com>
2021-09-09 21:27:30 +0000

    Deploying to gh-pages from  @ 5c24022a128de759d3deee675ccc42d3bd628d79 🚀

--- src/MLCourse.jl
+++ src/MLCourse.jl
@@ -8,8 +8,8 @@ include("notebooks.jl")
 include_dependency("../Project.toml")
 const _VERSION = VersionNumber(Pkg.TOML.parsefile(project_relative_path("Project.toml"))["version"])
 
-using Zygote, Plots, MLJ, MLJLinearModels, MLJGLMInterface, Markdown, DataFrames
-export plot_residuals!, fitted_linear_func, grid
+using Zygote, Plots, MLJ, MLJLinearModels, MLJGLMInterface, Markdown, DataFrames, Base64
+export plot_residuals!, fitted_linear_func, grid, biplot
 
 function plot_residuals!(x, y, f; kwargs...)
     for (xi, yi) in zip(x, y)
@@ -83,6 +83,44 @@ function MLJ.MLJBase.predict(model::PolynomialRegressor, fitresult, Xnew)
     MLJ.MLJBase.predict(model.regressor, fitresult, Xpoly)
 end
 
+function biplot(m; pc = 1:2)
+    scores = MLJ.transform(m, m.data[1])
+    p = scatter(getproperty(scores, Symbol(:x, pc[1])),
+                getproperty(scores, Symbol(:x, pc[2])),
+                label = nothing, aspect_ratio = 1, size = (600, 600),
+                xlabel = "PC$(pc[1])", ylabel = "PC$(pc[2])",
+                framestyle = :axis, markeralpha = 0,
+                txt = text.(1:nrows(scores), 8, :gray), markersize = 0)
+    plot!(p[1], inset = (1, bbox(0, 0, 1, 1)),
+          right_margin = 10Plots.mm, top_margin = 10Plots.mm)
+    p2 = p[1].plt.subplots[end]
+    plot!(p2, aspect_ratio = 1, mirror = true, legend = false)
+    params = fitted_params(m)
+    loadings = if hasproperty(params, :pca)
+        params.pca.projection
+    else
+        params.projection
+    end
+    n = names(m.data[1])
+    for i in 1:length(n)
+        plot!(p2, [0, .9*loadings[i, pc[1]]], [0, .9*loadings[i, pc[2]]],
+              c = :red, arrow = true)
+        annotate!(p2, [(loadings[i, pc[1]], loadings[i, pc[2]],
+                        (n[i], :red, :center, 8))])
+    end
+    scatter!(p2, 1.1*loadings[:, pc[1]], 1.1*loadings[:, pc[2]],
+             markersize = 0, markeralpha = 0) # dummy for sensible xlims and xlims
+    plot!(p2,
+          background_color_inside = RGBA{Float64}(0, 0, 0, 0),
+          tickfontcolor = RGB(1, 0, 0))
+    p
+end
+
+function embed_figure(name)
+    "![](data:img/png; base64,
+         $(open(base64encode,
+                project_relative_path("notebooks", "figures", name))))"
+end
 
 function start()
     sysimg = project_relative_path("precompile", "mlcourse.so")
