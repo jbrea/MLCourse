3d9a51e8ceb1987b36ec5a884cd59e2a6db16cfe
Johanni Brea <jbrea@users.noreply.github.com>
2021-09-18 12:43:50 +0200

    only first 2

--- src/notebooks.jl
+++ src/notebooks.jl
@@ -1,17 +1,17 @@
 const NOTEBOOKS = [("Introduction to Julia", "introduction.jl"),
                    ("Supervised Learning", "supervised_learning.jl"),
-                   ("Generalized Linear Regression", "generalized_linear_regression.jl"),
-                   ("Flexibility and Bias-Variance-Decomposition", "flexibility.jl"),
-                   ("Model Assessment", "model_evaluation.jl"),
-                   ("Regularization", "regularization.jl"),
-                   ("Transformations of Input or Output", "transformations.jl"),
-                   ("Gradient Descent", "gradient_descent.jl"),
-                   ("Multilayer Perceptrons", "mlp.jl"),
-                   ("Convolutional Neural Networks", "convnets.jl"),
-                   ("Recurrent Neural Networks", "rnn.jl"),
-                   ("Clustering", "clustering.jl"),
-                   ("Principal Component Analysis", "pca.jl"),
-                   ("Reinforcement Learning", "rl.jl")
+#                    ("Generalized Linear Regression", "generalized_linear_regression.jl"),
+#                    ("Flexibility and Bias-Variance-Decomposition", "flexibility.jl"),
+#                    ("Model Assessment", "model_evaluation.jl"),
+#                    ("Regularization", "regularization.jl"),
+#                    ("Transformations of Input or Output", "transformations.jl"),
+#                    ("Gradient Descent", "gradient_descent.jl"),
+#                    ("Multilayer Perceptrons", "mlp.jl"),
+#                    ("Convolutional Neural Networks", "convnets.jl"),
+#                    ("Recurrent Neural Networks", "rnn.jl"),
+#                    ("Clustering", "clustering.jl"),
+#                    ("Principal Component Analysis", "pca.jl"),
+#                    ("Reinforcement Learning", "rl.jl")
                   ]
 
 function _linkname(path, nb, basedir)
