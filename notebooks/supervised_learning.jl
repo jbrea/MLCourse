4c9763f44e2927686053f28c02b72eae52f67906
Johanni Brea <jbrea@users.noreply.github.com>
2021-09-18 12:42:29 +0200

    fix supervised learning

--- notebooks/supervised_learning.jl
+++ notebooks/supervised_learning.jl
@@ -23,23 +23,10 @@ end
 
 
 # ╔═╡ f63c04e8-eefe-11eb-39f6-83b31ebe73e7
-begin
-    using Plots
-    gr()
-end # with a begin ... end block we can write multiple lines in one cell
-
-
-# ╔═╡ f63c050e-eefe-11eb-08a9-b57452c4e44f
-using DataFrames, CSV
-
+using Plots, OpenML, DataFrames, CSV
 
 # ╔═╡ f63c0540-eefe-11eb-01d6-454a05f9182a
 using StatsPlots
-
-
-# ╔═╡ f63c0560-eefe-11eb-2617-f543fe2bdd5f
-using OpenML
-
 
 # ╔═╡ f63c0592-eefe-11eb-0d69-91d4d396c540
 using MLJ, MLJLinearModels
@@ -68,38 +55,33 @@ md"In this notebook we have a first look at some data sets and the [MLJ machine 
 mnist = OpenML.load(554) |> DataFrame;
 
 # ╔═╡ f63c04f2-eefe-11eb-2562-e530426e4300
-md"With the semicolon we omit output. If you want to know more about the syntax
-`data, labels = ` have a look at the [manual](https://docs.julialang.org/en/v1/manual/functions/#Multiple-Return-Values).
+md"With the semicolon we omit the output of a cell.
 "
-
 
 # ╔═╡ f63c04f2-eefe-11eb-3ceb-ff1e36a2a302
 mnist.class # labels is a vector of 70'000 integers between 0 and 9.
 
-
 # ╔═╡ f63c04fc-eefe-11eb-35b6-5345dda134e7
 size(mnist)
-
 
 # ╔═╡ f63c04fc-eefe-11eb-043c-7fec2fc41913
 md"The input of this data set consists of 70'000 grayscale images of 28x28 pixels.
 
 We can plot different input images with the following code.
+"
 
-Explanation: `data[19, 1:end-1]` gets all pixels of the 19th image.
+# ╔═╡ f63c0506-eefe-11eb-1857-e3eaf731c152
+plot(Gray.(reshape(Array(mnist[19, 1:end-1]) ./ 255, 28, 28)'))
+
+# ╔═╡ c82fb649-e60d-43fc-b158-444b27b6e9cf
+md"Explanation: `data[19, 1:end-1]` gets all pixels of the 19th image.
 Next we transform the dataframe row to an `Array`. In OpenML, the inputs are
  8-bit integer; we transform to values between 0 and 1 by dividing by 255.
 Then we reshape the vector of 784 entries to a matrix of size 28x28 with `reshape`.
 `'` transposes the matrix (alternatively we could use `PermutedDimsArray`; look it up in the Live docs, if you are interested). `Gray.(...)` applies the `Gray` function elementwise to all the pixels, to reinterpret these numbers as grayscale values."
 
-
-# ╔═╡ f63c0506-eefe-11eb-1857-e3eaf731c152
-plot(Gray.(reshape(Array(mnist[19, 1:end-1]) ./ 255, 28, 28)'))
-
-
 # ╔═╡ f63c0506-eefe-11eb-399f-455ee06548da
 mnist.class[19] # the label corresponding to image 19 is a 6 :)
-
 
 # ╔═╡ f63c050e-eefe-11eb-1709-0ddce2aee0ed
 Markdown.parse("## Spam Detection
@@ -116,26 +98,21 @@ preprocessing code in the scripts folder of the MLCourse package
 ($(normpath(joinpath(dirname(pathof(MLCourse)), "..", "scripts", "preprocess_spam.jl")))).
 ")
 
-
 # ╔═╡ f63c051a-eefe-11eb-0db2-c519292f88a2
 spam = CSV.read(joinpath(@__DIR__, "..", "data", "spam.csv"), DataFrame)
-
 
 # ╔═╡ f63c051a-eefe-11eb-16eb-a572fe41aa43
 spam[spam.label .== "ham", :] # select only ham
 
-
 # ╔═╡ f63c0524-eefe-11eb-3abd-63d677b12db9
 spam[spam.label .== "spam", :] # select only spam
-
 
 # ╔═╡ f63c0524-eefe-11eb-3732-258d6989e217
 md"## Wind Speed Prediction
 
 We load the weather data from a csv file on the harddisk to a DataFrame.
-Our goal is to predict the wind speed in Lucerne from 5 hours old pressure measurements.
+Our goal is to predict the wind speed in Lucerne from 5 hours old measurements.
 "
-
 
 # ╔═╡ f63c052e-eefe-11eb-3a14-e5e8f3d578a8
 weather = CSV.read(joinpath(@__DIR__, "..", "data", "weather2015-2018.csv"), DataFrame)[1:10000,:] # to speed up plotting, we use only the first 10'000 rows of the data set. Change `10000` to `end`, if you want to see all data.
@@ -154,7 +131,6 @@ histogram2d(X, y, markersize = 3, xlabel = "LUZ_pressure [hPa]",
 md"The [StatsPlots package](https://github.com/JuliaPlots/StatsPlots.jl) has some
 nice functions to explore data visually."
 
-
 # ╔═╡ f63c054c-eefe-11eb-13db-171084b417a9
 @df weather corrplot([:BAS_pressure :LUG_pressure :LUZ_pressure :LUZ_wind_peak],
                      grid = false, fillcolor = cgrad(), size = (700, 600))
@@ -165,18 +141,14 @@ md"## Other Data Sets
 There are many data sets collected by the [OpenML community](https://www.openml.org).
 We use them as follows."
 
-
 # ╔═╡ f63c0560-eefe-11eb-0251-2d0443448e66
 OpenML.list_datasets(output_format = DataFrame)
-
 
 # ╔═╡ f63c056a-eefe-11eb-199e-2b05b1385be4
 OpenML.describe_dataset(61)
 
-
 # ╔═╡ f63c056a-eefe-11eb-218c-afc51fdf1704
 iris = OpenML.load(61) |> DataFrame
-
 
 # ╔═╡ f63c0574-eefe-11eb-2d7c-bd74b7d83f23
 md"# Function Family
@@ -187,14 +159,11 @@ You can control the parameters ``\theta_0`` and ``\theta_1`` to find the
 function that visually matches the data well. The loss is the mean squared error. This corresponds to the average squared length of the red vertical lines. On the right we track the loss of the currently defined function.
 "
 
-
 # ╔═╡ f63c057e-eefe-11eb-1672-19db9efd0d32
 md"θ₀ = $(@bind θ₀ Slider(-1.8:.1:1.8, default = 0, show_value = true))"
 
-
 # ╔═╡ f63c057e-eefe-11eb-0dd2-3333b4e2d220
 md"θ₁ = $(@bind θ₁ Slider(-3:.1:3, default = 0, show_value = true))"
-
 
 # ╔═╡ f63c0588-eefe-11eb-0687-a7c44f9177a4
 let x = [0.62, 0.49, 0.28, 0.54, 0.0, 0.84, 0.29, 0.12, 0.49, 0.37, 0.36, 0.8, 0.63, 0.18, 0.2, 0.43, 0.68, 0.23, 0.72],
@@ -214,16 +183,13 @@ let x = [0.62, 0.49, 0.28, 0.54, 0.0, 0.84, 0.29, 0.12, 0.49, 0.37, 0.36, 0.8, 0
          layout = (1, 2), size = (700, 400))
 end
 
-
 # ╔═╡ f63c0588-eefe-11eb-1b0b-db185fcbdc4e
 md"# Linear Regression
 
 In the following we run in code the same example we had on the blackboard."
 
-
 # ╔═╡ f63c05a6-eefe-11eb-0ef4-df7e458e9ec6
 md"For now we use the same data set as in the blackboard example."
-
 
 # ╔═╡ f63c05a6-eefe-11eb-2927-b1e9dbbe032d
 training_data = DataFrame(x = [0., 2., 2.], y = [-1., 4., 3.])
@@ -236,13 +202,11 @@ begin
     plot!(x -> 2x - 1, label = "mean data generator")
 end
 
-
 # ╔═╡ f63c05b2-eefe-11eb-21dc-1f79ae779316
 md"Now we define a supervised learning machine. The function family, loss
    function and optimiser are implicitly defined by choosing `LinearRegressor()`.
    All machines for supervised learning have as arguments a method, the input
    and the output."
-
 
 # ╔═╡ f63c05ba-eefe-11eb-18b5-7522b326ab65
 mach = machine(LinearRegressor(),         # method
@@ -255,12 +219,11 @@ fit!(mach); # fit the machine
 # ╔═╡ f63c05c4-eefe-11eb-030e-6f7484849852
 fitted_params(mach) # show the result
 
-
 # ╔═╡ 25eb24a4-5414-481b-88d5-5ad50d75f8c9
 md"We can use the fitted machine and the `predict` function to compute ŷ (the predictions)."
 
 # ╔═╡ f0f6ac8f-2e61-4b08-9383-45ff5b02c5b1
-predict(mach, DataFrame(x = [0.5, 1.5]))
+predict(mach, DataFrame(x = [0.5, 1.5])) # the second argument is the new input.
 
 # ╔═╡ 7336e7b4-a7c2-4409-8d9d-00285cf633fb
 md"Next, we define a function to compute the mean squared error loss for a given machine `mach` and a data set `data`."
@@ -283,8 +246,9 @@ end; # this function extracts the fitted parameters and returns a function with 
 plot!(fitted_linear_func(mach), color = :green, label = "fit")
 
 # ╔═╡ f63c05e4-eefe-11eb-012a-9bae1d87f2b5
-md"Next we actually generate data from the data generator defined in the blackboard example. For reproducibility we will fix the seed of the random number generator."
+md"Next we actually generate data from the data generator defined in the blackboard example.
 
+For reproducibility we will fix the seed of the random number generator with the slider below. To do so we load the package `Random` first."
 
 # ╔═╡ f63c059c-eefe-11eb-208c-d5781711deb7
 md"With the sliders below you can define the noise level of the data generator, the number n of training points and the seed of the (pseudo-)random number generator.
@@ -317,17 +281,8 @@ begin
     plot!(fitted_linear_func(mach2), color = :green, label = "fit")
 end
 
-
 # ╔═╡ f63c0600-eefe-11eb-0023-9d5e5ddd987e
 (training_loss = L(mach2, generated_data), test_loss = L(mach2, generated_test_data))
-
-
-# ╔═╡ f63c060a-eefe-11eb-232d-ab1a9f3cd3bc
-md"**Exercise**
-
-Change the noise level σ, the size of the training data n and the seed with the sliders above and observe the training and the test losses. Write down your observations.
-"
-
 
 # ╔═╡ 0e6efce9-093a-4758-a649-6cff525711a5
 md"# Distributions
@@ -373,7 +328,6 @@ let
     plot(p1, p2, size = (700, 350), legend = false)
 end
 
-
 # ╔═╡ 5d3efbe5-8937-45c0-894d-d24ece9b2262
 md"## Categorical
 
@@ -398,7 +352,6 @@ md"# Logistic Regression (Linear Classification)"
 # ╔═╡ f63c0616-eefe-11eb-1cea-dfdaa64e6233
 classification_data = DataFrame(x = [0., 1., 1.], y = ["A", "B", "A"])
 
-
 # ╔═╡ f63c0616-eefe-11eb-1bfa-5b2ef11fcf4d
 coerce!(classification_data, :y => Multiclass) # with this we tell the computer to interpret the data in column y as multi-class data (in this case there are only 2 classes).
 
@@ -410,17 +363,15 @@ mach3 = machine(LogisticClassifier(penalty = :none),
 # ╔═╡ f63c061e-eefe-11eb-3b91-7136b4a16616
 predict(mach3, DataFrame(x = -1:.5:2)) # click on the little gray triangle above to toggle the display of the full output.
 
-
 # ╔═╡ f63c0628-eefe-11eb-3125-077e533456d9
 predict_mode(mach3, DataFrame(x = -1:.5:2))
 
-
 # ╔═╡ f63c0632-eefe-11eb-3b93-8549af7aaed9
-md"# Exercises
+md"""# Exercises
 
 ## Conceptual
-1. We have some training data ``((x_1 = 0, y_1 = -1), (x_2 = 2, y_2 = 4),
-   (x_3 = 2, y_3 = 3))`` and a family of probability densities
+1. We have some training data ``((x_1, y_1), (x_2, y_2), \ldots,
+   (x_n, y_n))`` and a family of probability densities
    ``p(y|x) = \frac1{\sqrt{2\pi\sigma^2}}\exp\left(-\frac{(y - \theta_0 - \theta_1 x)^2}{2\sigma^2}\right)``.
    - Write the log-likelihood function of the parameters ``\theta_0`` and ``\theta_1``
      for this data and model.
@@ -452,14 +403,15 @@ md"# Exercises
    - a distribution that could be used to model Y|X
 
 ## Applied
+1. Change the noise level σ, the size of the training data n and the seed with the sliders of the section "Linear Regression" and observe the training and the test losses. Write down your observations.
 1. Write a data generator function that samples inputs ``x`` normally distributed
    with mean 2 and standard deviation 3. The response ``y\in\{\mbox{true, false}\}``
    should be sampled from a Bernoulli distribution with rate of ``\mbox{true}``
-   equal to ``0.5x - 2.7``.
+   equal to ``\sigma(0.5x - 2.7)``.
    - Create a training set of size ``n = 50``.
    - Fit the data with logistic regression and look at the fitted parameters.
    - Repeat the 2 steps above with a training set of size ``n = 10^4``.
-"
+"""
 
 # ╔═╡ e3aa458a-486c-4d5b-a330-67fb68e6f516
 MLCourse.footer()
@@ -473,9 +425,9 @@ MLCourse.footer()
 # ╠═f63c04fc-eefe-11eb-35b6-5345dda134e7
 # ╟─f63c04fc-eefe-11eb-043c-7fec2fc41913
 # ╠═f63c0506-eefe-11eb-1857-e3eaf731c152
+# ╟─c82fb649-e60d-43fc-b158-444b27b6e9cf
 # ╠═f63c0506-eefe-11eb-399f-455ee06548da
 # ╟─f63c050e-eefe-11eb-1709-0ddce2aee0ed
-# ╠═f63c050e-eefe-11eb-08a9-b57452c4e44f
 # ╠═f63c051a-eefe-11eb-0db2-c519292f88a2
 # ╠═f63c051a-eefe-11eb-16eb-a572fe41aa43
 # ╠═f63c0524-eefe-11eb-3abd-63d677b12db9
@@ -488,7 +440,6 @@ MLCourse.footer()
 # ╠═f63c0540-eefe-11eb-01d6-454a05f9182a
 # ╠═f63c054c-eefe-11eb-13db-171084b417a9
 # ╟─f63c0560-eefe-11eb-0821-73afecf00357
-# ╠═f63c0560-eefe-11eb-2617-f543fe2bdd5f
 # ╠═f63c0560-eefe-11eb-0251-2d0443448e66
 # ╠═f63c056a-eefe-11eb-199e-2b05b1385be4
 # ╠═f63c056a-eefe-11eb-218c-afc51fdf1704
@@ -520,7 +471,6 @@ MLCourse.footer()
 # ╠═f63c05f6-eefe-11eb-0682-8d2651409f12
 # ╟─f63c0600-eefe-11eb-3779-df58afdb52ad
 # ╠═f63c0600-eefe-11eb-0023-9d5e5ddd987e
-# ╟─f63c060a-eefe-11eb-232d-ab1a9f3cd3bc
 # ╟─0e6efce9-093a-4758-a649-6cff525711a5
 # ╠═36a44a52-8a29-4f92-8136-2c66a81a1f5f
 # ╟─2adab164-1b4f-4f2f-ade3-dd70544c692e
@@ -528,7 +478,7 @@ MLCourse.footer()
 # ╟─8060861a-1931-4fae-b6ee-afb94ef2a3d5
 # ╟─6124e1eb-0aec-4ac9-aee6-383b25865220
 # ╟─5d3efbe5-8937-45c0-894d-d24ece9b2262
-# ╠═897fc69d-f1e9-49e9-9a61-25eb6849f7ec
+# ╟─897fc69d-f1e9-49e9-9a61-25eb6849f7ec
 # ╟─f63c0616-eefe-11eb-268a-271fdc2ddd98
 # ╠═f63c0616-eefe-11eb-1cea-dfdaa64e6233
 # ╠═f63c0616-eefe-11eb-1bfa-5b2ef11fcf4d
