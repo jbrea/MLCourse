2e5b2e0dbdfdabb07e7bab1d044cc22a0116d4d8
Johanni Brea <jbrea@users.noreply.github.com>
2022-01-28 11:16:37 +0100

    rm

--- plutoserver.py
+++
@@ -1,8 +0,0 @@
-def setup_plutoserver():
-  return {
-    "command": ["julia", "--sysimage=precompile/mlcourse.so", "-e", "import Pluto; Pluto.run(host=\"0.0.0.0\", port={port}, launch_browser=false, require_secret_for_open_links=false, require_secret_for_access=false, dismiss_update_notification=true, show_file_system=false)"],
-    "timeout": 60,
-    "launcher_entry": {
-        "title": "MLCourse",
-    },
-  }
