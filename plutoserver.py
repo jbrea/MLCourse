5cd4527359cbc8a3f91ef88f1d30c98d27dbef7c
Johanni Brea <jbrea@users.noreply.github.com>
2022-01-28 19:53:03 +0100

    fix?

--- plutoserver.py
+++
@@ -1,8 +0,0 @@
-def setup_plutoserver():
-  return {
-    "command": ["/bin/bash", "/home/jovyan/MLCourse/runpluto.sh", "{port}"],
-    "timeout": 60,
-    "launcher_entry": {
-        "title": "Pluto.jl",
-    },
-  }
