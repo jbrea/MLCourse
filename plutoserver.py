e5b578ca28d863d43aaed60250f2049105325a6b
Johanni Brea <jbrea@users.noreply.github.com>
2022-01-28 16:45:00 +0100

    fix?

--- plutoserver.py
+++ plutoserver.py
@@ -1,6 +1,6 @@
 def setup_plutoserver():
   return {
-    "command": ["julia", "--optimize=0", "-e", "import Pluto; Pluto.run(host=\"0.0.0.0\", port={port}, launch_browser=false, require_secret_for_open_links=false, require_secret_for_access=false, dismiss_update_notification=true, show_file_system=false)"],
+    "command": ["/usr/local/bin/julia", "--optimize=0", "-e", "import Pluto; Pluto.run(host=\"0.0.0.0\", port={port}, launch_browser=false, require_secret_for_open_links=false, require_secret_for_access=false, dismiss_update_notification=true, show_file_system=false)"],
     "timeout": 60,
     "launcher_entry": {
         "title": "MLCourse",
