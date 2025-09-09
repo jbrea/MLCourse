a7c9b521ac89093e19025ba9c0461b6d9234ed01
Johanni Brea <jbrea@users.noreply.github.com>
2025-09-09 19:35:19 +0200

    trigger rebuild

--- notebooks/generalized_linear_regression.jl
+++ notebooks/generalized_linear_regression.jl
@@ -1,19 +1,17 @@
 ### A Pluto.jl notebook ###
-# v0.20.17
+# v0.19.45
 
 using Markdown
 using InteractiveUtils
 
 # This Pluto notebook uses @bind for interactivity. When running this notebook outside of Pluto, the following 'mock version' of @bind gives bound variables a default value (instead of an error).
 macro bind(def, element)
-    #! format: off
-    return quote
+    quote
         local iv = try Base.loaded_modules[Base.PkgId(Base.UUID("6e696c72-6542-2067-7265-42206c756150"), "AbstractPlutoDingetjes")].Bonds.initial_value catch; b -> missing; end
         local el = $(esc(element))
         global $(esc(def)) = Core.applicable(Base.get, el) ? Base.get(el) : iv(el)
         el
     end
-    #! format: on
 end
 
 # ╔═╡ 94f8e29e-ef91-11eb-1ae9-29bc46fa505a
@@ -1027,6 +1025,7 @@ end
 
 # ╔═╡ 9d250061-e570-4537-b1aa-f6a9019f343d
 MLCourse.save_cache(@__FILE__)
+
 
 # ╔═╡ Cell order:
 # ╟─661e96c4-9432-4e39-b67e-6d0de2b76635
