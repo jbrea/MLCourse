e76aff954c469831eaf322ec6a80aea27ac8a422
Johanni Brea <jbrea@users.noreply.github.com>
2025-09-24 17:10:30 +0200

    trigger rebuild

--- notebooks/introduction.jl
+++ notebooks/introduction.jl
@@ -404,6 +404,7 @@ MLCourse.FOOTER
 # ╔═╡ 9d250061-e570-4537-b1aa-f6a9019f343d
 MLCourse.save_cache(@__FILE__)
 
+
 # ╔═╡ Cell order:
 # ╟─f3508747-da29-47c9-a98e-22ea15caaf2f
 # ╟─7343bb19-ed3f-4db1-be83-ca165099dcc3
